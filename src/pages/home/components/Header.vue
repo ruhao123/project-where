a48cdb4ab5c30aa95463169e514888f2b94f50bc
ruhao123 <2668667332@qq.com>
2019-10-22 10:45:15 +0800

    frist submit

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+<div  class="header">
+   <div class="header-left">返回</div>
+   <div class="header-input">内容</div>
+   <div class="header-right">城市</div>
+</div>
+ 
+</template>
+
+<script>
+export default {
+name:"HomeHeader"
+}
+</script>
+
+<style lang="stylus" scoped>
+display:flex
+.header
+height: .86rem
+.header-left
+height: .64rem
+float:left
+.header-input
+flex:1
+
+.header-right
+height: 1.24rem
+float:right
+
+
+</style>
